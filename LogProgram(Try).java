216aaddc1f4d3748ff7667d8f684f9e23c6e9ea0
HeejoSuh <15654639+HeejoSuh@users.noreply.github.com>
2018-02-10 19:38:03 -0500

    Update and rename LogProgram.java to LogProgram(Try).java
    Program does not work for string inputs.

--- LogProgram(Try).java
+++ LogProgram(Try).java
@@ -7,6 +7,7 @@
  * This program calculates the number of logs that will fit on a truck,
  *     if you know their length
  *     
+ * THIS DOES NOT WORK FOR STRING INPUTS
  *
  ****************************************************************************/
 
